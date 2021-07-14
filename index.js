4ccd6b49c81120c27626a0cc411493a2ebb0dc40
Rohan Verma <rohanvermaara@gmail.com>
2021-07-14 17:19:59 +0530

    heroku deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 app.listen(PORT, () => {
